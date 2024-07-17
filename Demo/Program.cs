ebd06f22547f9e90dc5b3ac14235241acec4bada
unknown01142 <hanyba06@gmail.com>
2024-07-17 18:08:32 +0300

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Demo.Overloading_Operator;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+        }
+    }
+}
